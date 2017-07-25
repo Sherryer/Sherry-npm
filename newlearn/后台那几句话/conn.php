c7b34dfd916afefed8b6739087aa1d1e1797c4cb
Sherry Christain <fll_008@sina.com>
2017-07-25 16:44:51 +0800

    clear master

--- newlearn/后台那几句话/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-//建立php与数据库服务器连接,并返回连接对象
-$conn=@mysql_connect("localhost","root","") or die("db connect error!");
-//切换到指定数据库wz21
-mysql_select_db("wz21",$conn);
-//设置支持中文
-mysql_query("set names utf8");
-
-?>
