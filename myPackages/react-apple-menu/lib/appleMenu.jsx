7cb42709159f1d34db33ed86ca20a7c643109aba
Sherry Christain <fll_008@sina.com>
2017-08-22 23:54:53 +0800

    雏形

--- myPackages/react-apple-menu/lib/appleMenu.jsx
+++ myPackages/react-apple-menu/lib/appleMenu.jsx
@@ -5,14 +5,43 @@ import React, {Component} from 'react';
 import style from './style.css'
 
 class AppleMenu extends Component {
+    constructor(...props){
+        super(...props);
+        this.getDistance = this.getDistance.bind(this);
+        this.mouseMove = this.mouseMove.bind(this);
+    }
+
+    mouseMove (ev) {
+        let oEvent = ev || event;
+        let target = this.refs.target;
+        let aImg = target.getElementsByTagName('img');
+        let d = 0;
+        let iMax = 200;
+        let i = 0;
+
+        for (i = 0; i < aImg.length; i++) {
+            d = this.getDistance(aImg[i], target, oEvent);
+            d = Math.min(d, iMax);
+
+            aImg[i].style.width = ((iMax - d) / iMax) * 64 + 64+'px';
+        }
+    };
+
+    getDistance(img, target, oEvent) {
+        return Math.sqrt(
+            Math.pow(img.offsetLeft + target.offsetLeft - oEvent.clientX + img.offsetWidth / 2, 2) +
+            Math.pow(img.offsetTop + target.offsetTop - oEvent.clientY + img.offsetHeight / 2, 2)
+        );
+    }
+
     render () {
         return (
-            <div className={style.content}>
-                <img src="images/1.png"/>
-                <img src="images/2.png"/>
-                <img src="images/3.png"/>
-                <img src="images/4.png"/>
-                <img src="images/5.png"/>
+            <div ref="target" onMouseMove={this.mouseMove} className={style.content}>
+                <img src="https://raw.githubusercontent.com/Sherryer/Sherry-npm/master/static/images/1.png"/>
+                <img src="https://raw.githubusercontent.com/Sherryer/Sherry-npm/master/static/images/2.png"/>
+                <img src="https://raw.githubusercontent.com/Sherryer/Sherry-npm/master/static/images/3.png"/>
+                <img src="https://raw.githubusercontent.com/Sherryer/Sherry-npm/master/static/images/4.png"/>
+                <img src="https://raw.githubusercontent.com/Sherryer/Sherry-npm/master/static/images/5.png"/>
             </div>
         )
     }
